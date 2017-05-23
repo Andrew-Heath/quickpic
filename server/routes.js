8c795adc2e38655c37e4c5fd8807b74d2bc0244f
Andrew Heath <aheath42@gmail.com>
2017-05-22 20:10:28 -0700

    Fix user endpoint handler to redirect on unwanted access

--- server/routes.js
+++ server/routes.js
@@ -9,7 +9,9 @@ module.exports = (app) => {
   app.get('/users/*', (req, res) => {
     console.log(req.url);
     var endPoint = req.url.slice(7);
-    if(typeof Number(endPoint) === 'number') {
+    console.log('endpoint = ' + endPoint);
+    if(endPoint.length > 0 && Number(endPoint) > -1) {
+      console.log('in number');
       res.send(users[endPoint])
     } else {
       res.redirect('/');

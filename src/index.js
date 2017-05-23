818624170649d6d16ef4638dd194fce29392c564
Andrew Heath <aheath42@gmail.com>
2017-05-22 17:00:03 -0700

    Change to react-router-dom, update index.js for new version of react-router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Component Imports
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+// Redux related imports
 import {Provider} from 'react-redux';
 import configureStore from './store/configure-store';
 import './index.css';
+
+// import registerServiceWorker from './registerServiceWorker';
 
 const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <div>
+      <App />
+      <Router>
+        <Route exact path='/' component={App}/>
+      </Router>
+    </div>
   </Provider>,
   document.getElementById('app')
 );

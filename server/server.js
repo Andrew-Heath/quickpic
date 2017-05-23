a80b7b405ed0c8f0dd08ed7917d5da88a0e57934
Andrew Heath <aheath42@gmail.com>
2017-05-22 19:22:06 -0700

    Update server to es6

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 app.use(express.static(__dirname + '/../build/'));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/../build/index.html');
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
 });

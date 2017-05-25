982f1a73397e5b45fd34ead1c2ec4249067727f8
Andrew Heath <aheath42@gmail.com>
2017-05-25 16:25:11 -0700

    Organize landing page CSS to allow for dual column layout, reusable format as well

--- src/components/landing.js
+++ src/components/landing.js
@@ -6,7 +6,12 @@ class Landing extends Component {
   render() {
     return (
       <div className="landing">
-        This is the landing.
+        <div className="col-wrapper" >
+          <div className="col-2 col-left">Advertisement or some kind of general image selection to show potential users. Could include popular posts by users or information on recent updates.</div>
+          <div className="col-2 col-right">Sign Up box<br />
+          Page divider<br />
+          Login box</div>
+        </div>
       </div>
     );
   }

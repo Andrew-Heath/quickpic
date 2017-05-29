8b4fb2657db5960c86deaae23c4b0f3340599463
Andrew Heath <aheath42@gmail.com>
2017-05-29 14:32:53 -0700

    Create file for user profile page

---
+++ src/components/user-prof.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// this header component will appear on every page
+// pass in the Link component of React-Route to properly link to other pages
+class User extends Component {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default User;
